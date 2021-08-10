de067378d9860773cd610bccce24db7805e53d9c
arajput96 <sachem2015@gmail.com>
2021-08-10 15:51:22 -0400

    Installed Babel Related Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,25 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-  mode: 'production', 
+  mode: 'development', 
+  module: {
+    rules: [
+      { 
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader:'babel-loader',
+          options:{
+            presets:['@babel/preset-env','@babel/preset-react'],
+        }
+      }},
+      {
+        test: /\.s[ac]ss$/i,
+        use: [ "style-loader", "css-loader", "sass-loader" ], 
+      }
+    ]
+  },//this closes module property
+  devServer: {
+    publicPath: '/build/',
+  }
 };
